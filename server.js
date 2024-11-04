de0ccda328b7b048db1c736e606ea55a3587943b
CantonPo <cantoncorpas@gmail.com>
2024-11-04 11:11:23 +0100

    22

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
+const express = require("express")
 const { exec } = require('child_process');
-const app = express();
-const PORT = 3002;
+
+//const comando = 'touch test.txt';
+//const comando = 'echo "hola mundo" > test.txt';
+//const comando = "ping -c 4 google.com"
+//const comando = "figlet hola mundo"
+const app = express()
+const port = 3003
+
+app.use(express.static("public"))
+
+app.get("/", (req,res) => {
+    exec(comando, (error, stdout, stderr) => {
+        res.send(`<pre>${stdout}</pre>`)
+      });
+})
 
 app.get("/ping", (req, res) => {
-    const comando = "ping -c 4 mercadona.es"; // Asegúrate de que esta dirección sea correcta
+    const dominio = req.query.dominio
+    const comando = "ping -c 4 " + dominio
     exec(comando, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error: ${error.message}`);
-            return res.status(500).send(`Error: ${error.message}`);
-        }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return res.status(500).send(`stderr: ${stderr}`);
-        }
-        res.send(`<pre>${stdout}</pre>`); // Devuelve la salida del comando
+        res.send(stdout)
     });
-});
+})
 
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+app.listen(port, () => {
+    console.log(`Servidor funcionando en ${port}`)
+})
